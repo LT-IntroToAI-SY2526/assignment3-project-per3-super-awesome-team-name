916eddf200579dcaf1a8c7bc0cf181b59e322f14
NoahK101424 <njkim1@cps.edu>
2025-10-17 10:29:47 -0500

    question functions

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # What should I make for % ? (event)
 # What can I make that includes %? (specific ingredient)
 # What dish had a serving size of _? (serving size)
-# What dish has a cooking time under/over _? (Cooking time)
-# What dish has under/over _ steps? (cooking complexity/difficulty level)
+# What dish has a cooking time of _ minutes? (Cooking time)
+# What dish has _ steps? (cooking complexity/difficulty level)
 # What dish can I make for %? (breakfast, lunch, dinner, dessert)
 # bye
 
@@ -69,5 +69,72 @@ def ingredient_to_recipe(matches: List[str]) -> List[str]:
     result = []
     for recipe in recipe_db:
         if get_ingredients(recipe) == matches[0]:
+            result.append(get_name(recipe))
+    return result
+
+def serving_size(matches: List[str]) -> List[str]:
+    """After getting the input of a specififc serving size return the name of all dishes that match that serving size
+    
+        Args:
+        matches - a list of 1 string, just the serving size . Make sure to convert str -> int
+        
+    Returns:
+            a list of recipes that have that serving size"""
+    
+    result = []
+    size = int(matches[0])
+
+    for recipe in recipe_db:
+        if get_size(recipe) == size:
+            result.append(get_name(recipe))
+    return result
+
+def cook_time(matches: List[str]) -> List[str]:
+    """After getting the input of a specififc time (in minutes) return the name of all dishes that match that cook time
+    
+        Args:
+        matches - a list of 1 string, just the cooking time . Make sure to convert str -> int
+        
+    Returns:
+            a list of recipes that have that cookin time"""
+    
+    result = []
+    time = int(matches[0])
+
+    for recipe in recipe_db:
+        if get_time(recipe) == time:
+            result.append(get_name(recipe))
+    return result
+
+def num_steps(matches: List[str]) -> List[str]:
+    """After getting the input of a specififc number of steps return the name of all dishes that match that number of steps
+    
+        Args:
+        matches - a list of 1 string, just the number of steps. Make sure to convert str -> int
+        
+    Returns:
+            a list of recipes that have that number of steps"""
+    
+    result = []
+    steps = int(matches[0])
+
+    for recipe in recipe_db:
+        if get_steps(recipe) == steps:
+            result.append(get_name(recipe))
+    return result
+
+def type_of_meal(matches:List[str]) -> List[str]:
+    """After getting the input of a specififc type of meal (breakfast, lunch, dinner, dessert) return the name of all dishes that match
+    
+        Args:
+        matches - a list of 1 string, just the type of meal
+        
+    Returns:
+            a list of recipes that are that type of meal"""
+    
+    result = []
+
+    for recipe in recipe_db:
+        if get_mealType(recipe) == matches[0]:
             result.append(get_name(recipe))
     return result
